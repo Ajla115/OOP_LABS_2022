05b81c3afd2e25cc4d584fbcda4c25692474be9d
Ajla Korman <116031499+Ajla115@users.noreply.github.com>
2022-12-05 13:30:03 +0100

    Delete Task8.java

--- Task8.java
+++
@@ -1,8 +0,0 @@
-package com.Week6;
-
-public class Task8 {
-    public static void main(String[] args) {
-        NightSky sky = new NightSky(0.1, 40, 10);
-        sky.print();
-        System.out.println("Number of stars: " + sky.starsInLastPrint());
-    }}
